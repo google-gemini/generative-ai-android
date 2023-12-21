097e047a0c22c8d91722a014e0b6fa86c01a4775
5peak2me <461260911@qq.com>
2023-12-21 10:05:02 +0800

    Use the same version of gradle-wrapper and agp.

--- generativeai-android-sample/build.gradle.kts
+++ generativeai-android-sample/build.gradle.kts
@@ -20,7 +20,7 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0" apply false
+    id("com.android.application") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
