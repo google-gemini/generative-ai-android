52d28c6d8392af8b8d5df518e6d2a9b06d4838e3
Daymon <daymxn@google.com>
2024-04-03 17:50:42 -0500

    Use child directory for api files

--- plugins/src/main/java/com/google/gradle/plugins/ApiPlugin.kt
+++ plugins/src/main/java/com/google/gradle/plugins/ApiPlugin.kt
@@ -69,7 +69,7 @@ abstract class ApiPlugin : Plugin<Project> {
 
   context(Project)
   private fun ApiPluginExtension.commonConfiguration() {
-    val latestApiFile = rootProject.file("api/${project.version}.api")
+    val latestApiFile = rootProject.file("api/${project.name}/${project.version}.api")
 
     apiFile.convention(latestApiFile)
   }
