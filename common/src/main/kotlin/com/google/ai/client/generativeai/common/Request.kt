9c4aaf0b224a1159a06d8ae7c61d32868dfe1bfc
David Motsonashvili <davidmotson@gmail.com>
2024-06-10 10:08:34 -0700

    Make CountTokenRequest contain the full request (#152)
    Co-authored-by: David Motsonashvili <davidmotson@google.com>

--- common/src/main/kotlin/com/google/ai/client/generativeai/common/Request.kt
+++ common/src/main/kotlin/com/google/ai/client/generativeai/common/Request.kt
@@ -21,15 +21,15 @@ import com.google.ai.client.generativeai.common.client.Tool
 import com.google.ai.client.generativeai.common.client.ToolConfig
 import com.google.ai.client.generativeai.common.shared.Content
 import com.google.ai.client.generativeai.common.shared.SafetySetting
+import com.google.ai.client.generativeai.common.util.fullModelName
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
 
 sealed interface Request
 
 @Serializable
 data class GenerateContentRequest(
-  @Transient val model: String? = null,
+  val model: String? = null,
   val contents: List<Content>,
   @SerialName("safety_settings") val safetySettings: List<SafetySetting>? = null,
   @SerialName("generation_config") val generationConfig: GenerationConfig? = null,
@@ -39,5 +39,28 @@ data class GenerateContentRequest(
 ) : Request
 
 @Serializable
-data class CountTokensRequest(@Transient val model: String? = null, val contents: List<Content>) :
-  Request
+data class CountTokensRequest(
+  val generateContentRequest: GenerateContentRequest? = null,
+  val model: String? = null,
+  val contents: List<Content>? = null,
+  val tools: List<Tool>? = null,
+  @SerialName("system_instruction") val systemInstruction: Content? = null,
+) : Request {
+  companion object {
+    fun forGenAI(generateContentRequest: GenerateContentRequest) =
+      CountTokensRequest(
+        generateContentRequest =
+          generateContentRequest.model?.let {
+            generateContentRequest.copy(model = fullModelName(it))
+          } ?: generateContentRequest
+      )
+
+    fun forVertexAI(generateContentRequest: GenerateContentRequest) =
+      CountTokensRequest(
+        model = generateContentRequest.model?.let { fullModelName(it) },
+        contents = generateContentRequest.contents,
+        tools = generateContentRequest.tools,
+        systemInstruction = generateContentRequest.systemInstruction,
+      )
+  }
+}
