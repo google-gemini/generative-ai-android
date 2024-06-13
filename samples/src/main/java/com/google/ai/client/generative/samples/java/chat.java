cfb13310e03821264b3905a659b22786bf030fd5
Rodrigo Lazo Paz <rlazo@google.com>
2024-06-13 10:49:20 -0400

    Use right package name for java snippets

--- samples/src/main/java/com/google/ai/client/generative/samples/java/chat.java
+++ samples/src/main/java/com/google/ai/client/generative/samples/java/chat.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.ai.client.generative.samples;
+package com.google.ai.client.generative.samples.java;
 
 class Chat {
   void chat() {
