ff2b8bbefc0654d889ab33c1f19049e04cbfbda7
David Motsonashvili <davidmotson@gmail.com>
2024-07-12 03:56:44 -0700

    add jvmField annotation to Tool.CODE_EXECUTION (#201)
    Co-authored-by: David Motsonashvili <davidmotson@google.com>

--- generativeai/src/main/java/com/google/ai/client/generativeai/type/Tool.kt
+++ generativeai/src/main/java/com/google/ai/client/generativeai/type/Tool.kt
@@ -30,6 +30,6 @@ class Tool(
   val codeExecution: JSONObject? = null,
 ) {
   companion object {
-    val CODE_EXECUTION = Tool(codeExecution = JSONObject())
+    @JvmField val CODE_EXECUTION = Tool(codeExecution = JSONObject())
   }
 }
