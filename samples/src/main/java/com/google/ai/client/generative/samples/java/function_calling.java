b1855edf0990550cae0b26254807a1c38c8ec119
Rodrigo Lazo Paz <rlazo@google.com>
2024-07-12 07:45:39 -0400

    Initial java sample for function calling

--- samples/src/main/java/com/google/ai/client/generative/samples/java/function_calling.java
+++ samples/src/main/java/com/google/ai/client/generative/samples/java/function_calling.java
@@ -21,6 +21,50 @@ package com.google.ai.client.generative.samples.java;
 // the "Set up your API Key section" in the [Gemini API
 // quickstart](https://ai.google.dev/gemini-api/docs/quickstart?lang=android#set-up-api-key).
 
+import static com.google.ai.client.generativeai.type.FunctionDeclarationsKt.defineFunction;
+
+import com.google.ai.client.generativeai.GenerativeModel;
+import com.google.ai.client.generativeai.java.GenerativeModelFutures;
+import com.google.ai.client.generativeai.type.Content;
+import com.google.ai.client.generativeai.type.FunctionDeclaration;
+import com.google.ai.client.generativeai.type.Schema;
+import java.util.Arrays;
+
 class FunctionCalling {
-  // TODO
+
+  void functionCalling() {
+    // [START function_calling]
+    FunctionDeclaration multiplyDefinition =
+        defineFunction(
+            /* name  */ "multiply",
+            /* description */ "returns a * b.",
+            /* parameters */ Arrays.asList(
+                Schema.numDouble("a", "First parameter"),
+                Schema.numDouble("b", "Second parameter")),
+            /* required */ Arrays.asList("a", "b"));
+
+    // Specify a Gemini model appropriate for your use case
+    GenerativeModel gm =
+        new GenerativeModel(
+            /* modelName */ "gemini-1.5-flash",
+            // Access your API key as a Build Configuration variable (see "Set up your API key"
+            // above)
+            /* apiKey */ BuildConfig.apiKey);
+    GenerativeModelFutures model = GenerativeModelFutures.from(gm);
+
+    // (optional) Create previous chat history for context
+    Content.Builder userContentBuilder = new Content.Builder();
+    userContentBuilder.setRole("user");
+    userContentBuilder.addText("Hello, I have 2 dogs in my house.");
+    Content userContent = userContentBuilder.build();
+
+    Content.Builder modelContentBuilder = new Content.Builder();
+    modelContentBuilder.setRole("model");
+    modelContentBuilder.addText("Great to meet you. What would you like to know?");
+    Content modelContent = userContentBuilder.build();
+
+    // List<Content> history = Arrays.asList(userContent, modelContent);
+
+    // [END function_calling]
+  }
 }
