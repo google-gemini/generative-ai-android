37dd6be0710501204976a15babb6b48da446a0d1
Tanzim Hossain <tanzim@google.com>
2024-07-16 21:42:31 -0400

    make args nullable

--- common/src/main/kotlin/com/google/ai/client/generativeai/common/shared/Types.kt
+++ common/src/main/kotlin/com/google/ai/client/generativeai/common/shared/Types.kt
@@ -64,7 +64,7 @@ data class CodeExecutionResultPart(val codeExecutionResult: CodeExecutionResult)
 
 @Serializable data class FunctionResponse(val name: String, val response: JsonObject)
 
-@Serializable data class FunctionCall(val name: String, val args: Map<String, String?>)
+@Serializable data class FunctionCall(val name: String, val args: Map<String, String?>? = null)
 
 @Serializable data class FileDataPart(@SerialName("file_data") val fileData: FileData) : Part
 
