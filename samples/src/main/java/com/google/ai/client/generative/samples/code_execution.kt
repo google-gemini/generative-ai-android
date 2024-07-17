3720710c9748f63849aa80eef24fd8878b7ad587
Rodrigo Lazo <rlazo@users.noreply.github.com>
2024-07-17 15:33:46 -0400

    Update samples/src/main/java/com/google/ai/client/generative/samples/code_execution.kt
    Co-authored-by: Daymon <17409137+daymxn@users.noreply.github.com>

--- samples/src/main/java/com/google/ai/client/generative/samples/code_execution.kt
+++ samples/src/main/java/com/google/ai/client/generative/samples/code_execution.kt
@@ -66,7 +66,7 @@ suspend fun codeExecutionChat() {
     // Each `part` either contains `text`, `executable_code` or an `execution_result`
     println(response.candidates[0].content.parts.joinToString("\n"))
 
-    // Alternatively, you can use the `text` accessor joins the parts into a markdown compatible
+    // Alternatively, you can use the `text` accessor which joins the parts into a markdown compatible
     // text representation
     println(response.text)
     // [END code_execution_chat]
